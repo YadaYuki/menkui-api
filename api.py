c5864bbff0a5cde0430a7a07f60f71a5444c4147
YadaYuki <yada.yuki@fuji.waseda.jp>
2020-10-29 21:41:05 +0900

    add s3 uploader and url fetch api

--- api.py
+++ api.py
@@ -8,17 +8,22 @@ app = Flask(__name__)
 @app.route("/get_menkui",methods=["POST","GET"])
 def get_menkui():
     # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-presigned-urls.html
-    session = boto3.session.Session(profile_name="yadayuki")
-    s3_client = session.client("s3")
+    if os.environ.get("IS_LOCAL") == "true":
+        session = boto3.session.Session(profile_name=os.environ.get("PROFILE_NAME"))
+        s3_client = session.client("s3")
+    else:
+        s3_client = boto3.client("s3") 
+    
     image_bucket_name = os.environ.get("IMAGE_BUCKET_NAME")
-    res = s3_client.put_object(
+    key = "hogehoge.txt"
+    s3_client.put_object(
         Body="file",
         Bucket=image_bucket_name,
-        Key="gehogehoge.txt"
+        Key=key
     )
+    url = s3_client.generate_presigned_url('get_object',Params={'Bucket': image_bucket_name,'Key': key})
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(url)
     }
-
-    return response
+    return url

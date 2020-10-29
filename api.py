7c80258878b365ea10404be7024b4ba00d35da77
YadaYuki <yada.yuki@fuji.waseda.jp>
2020-10-29 20:37:34 +0900

    add boto3

--- api.py
+++ api.py
@@ -1,25 +1,21 @@
 import json
-# import boto3
+import boto3
 import os
 from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route("/get_menkui",methods=["POST"])
+@app.route("/get_menkui",methods=["POST","GET"])
 def get_menkui():
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": "hhogehoge"
-    }
     # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-presigned-urls.html
-    # session = boto3.session.Session(profile_name="yadayuki")
-    # s3_client = session.client("s3")
-    # image_bucket_name = os.environ.get("IMAGE_BUCKET_NAME")
-    # res = s3_client.put_object(
-    #     Body="file",
-    #     Bucket=image_bucket_name,
-    #     Key="sample.txt"
-    # )
+    session = boto3.session.Session(profile_name="yadayuki")
+    s3_client = session.client("s3")
+    image_bucket_name = os.environ.get("IMAGE_BUCKET_NAME")
+    res = s3_client.put_object(
+        Body="file",
+        Bucket=image_bucket_name,
+        Key="gehogehoge.txt"
+    )
     response = {
         "statusCode": 200,
         "body": json.dumps(body)

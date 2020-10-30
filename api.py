c098982d1357f094524fd21d612cdf95d836a7c3
YadaYuki <yada.yuki@fuji.waseda.jp>
2020-10-30 11:22:55 +0900

    add file upload

--- api.py
+++ api.py
@@ -8,22 +8,23 @@ app = Flask(__name__)
 @app.route("/get_menkui",methods=["POST","GET"])
 def get_menkui():
     # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-presigned-urls.html
+
+    file = request.files["image"]
+    key = file.filename
+    #TODO: EncType
+    #get image data
     if os.environ.get("IS_LOCAL") == "true":
         session = boto3.session.Session(profile_name=os.environ.get("PROFILE_NAME"))
         s3_client = session.client("s3")
     else:
         s3_client = boto3.client("s3") 
-    
+    # TODO: filename encoding
     image_bucket_name = os.environ.get("IMAGE_BUCKET_NAME")
-    key = "hogehoge.txt"
     s3_client.put_object(
-        Body="file",
+        Body=file.read(),
         Bucket=image_bucket_name,
         Key=key
     )
     url = s3_client.generate_presigned_url('get_object',Params={'Bucket': image_bucket_name,'Key': key})
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(url)
-    }
+
     return url

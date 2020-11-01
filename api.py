7fc0b3d9ba94859c0cbd8290a07dc206db858b62
YadaYuki <yada.yuki@fuji.waseda.jp>
2020-11-01 13:07:40 +0900

    add face rec

--- api.py
+++ api.py
@@ -1,16 +1,19 @@
 import json
 import boto3
+import cv2
+import numpy as np
+from PIL import Image
 import os
 from flask import Flask,request
+import io
+from utils.face import mosaic,get_face_positions,get_format
 
 app = Flask(__name__)
 
 @app.route("/get_menkui",methods=["POST","GET"])
 def get_menkui():
     # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-presigned-urls.html
-
-    file = request.files["image"]
-    key = file.filename
+    
     #TODO: EncType
     #get image data
     if os.environ.get("IS_LOCAL") == "true":
@@ -19,12 +22,30 @@ def get_menkui():
     else:
         s3_client = boto3.client("s3") 
     # TODO: filename encoding
+    # TODO: request type validation(method,file,filetype) and accurate zres code 
+    file = request.files["image"]
+    key = file.filename
+    image_data = file.read()
+    # 
+    img = np.array(Image.open(io.BytesIO(image_data))) 
+    faces = get_face_positions(img)
+
+    for (x, y, w, h) in faces:
+        img = mosaic(img,x,y,w,h)
+    
+    # Convert Numpy array to Binary
+    mosaiced_image_pillow = Image.fromarray(img)
+    byte_stream = io.BytesIO()
+    mosaiced_image_pillow.save(byte_stream,format=get_format(file.content_type))
+
     image_bucket_name = os.environ.get("IMAGE_BUCKET_NAME")
     s3_client.put_object(
-        Body=file.read(),
+        Body=byte_stream.getvalue(),
         Bucket=image_bucket_name,
-        Key=key
+        Key=key,
+        ContentType=file.content_type
     )
+
     url = s3_client.generate_presigned_url('get_object',Params={'Bucket': image_bucket_name,'Key': key})
 
     return url

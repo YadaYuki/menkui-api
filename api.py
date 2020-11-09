80a401c9cdfcabdf85c16159e9d0a9d484a82956
YadaYuki <yada.yuki@fuji.waseda.jp>
2020-11-09 12:29:23 +0900

    add cors

--- api.py
+++ api.py
@@ -4,29 +4,34 @@ import cv2
 import numpy as np
 from PIL import Image
 import os
-from flask import Flask,request
+from flask import Flask,request,make_response
 import io
 from utils.face import mosaic,get_face_positions,get_format
 from botocore.exceptions import ClientError
 
 app = Flask(__name__)
 
+
+def get_response(body,status_code):
+    # TODO:set cors origin 
+    r = make_response((json.dumps(body),status_code,{'Access-Control-Allow-Origin':'*'}))
+    return r
+
 @app.route("/get_menkui",methods=["POST"])
 def get_menkui():
     # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-presigned-urls.html
     ALLOWED_FILE_TYPE = ["image/jpeg","image/png"]
-    if request.methods != "POST":
-        return {"error_msg":"request method is invalid"},405
+    if request.method != "POST":
+        return get_response({"error_msg":"request method is invalid"},405)
     #TODO: EncType
     #get image data
-    
     # TODO: filename encoding
-    file = request.files["image"]
+    file = request.files.get("image")
     if not file:
-        return {"error_msg":"file is empty"},400
+        return get_response({"error_msg":"file is empty"},401)
         
     if not(file.content_type in ALLOWED_FILE_TYPE):
-        return {"error_msg":"file format is not accepted"},400
+        return get_response({"error_msg":"file format is not accepted"},400)
 
     image_data = file.read()
     # 
@@ -59,6 +64,5 @@ def get_menkui():
         url = s3_client.generate_presigned_url('get_object',Params={'Bucket': image_bucket_name,'Key': key},ExpiresIn=604800) # 7 days
     except ClientError as e:
         print(e)
-        return {"error_msg":"failed put object to s3"},500
-
-    return url,201
+        return get_response({"error_msg":"failed put object to s3"},500)
+    return get_response(url,201)
